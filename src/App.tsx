cd6d865257f32c36d17b4f10740f0be2fb2754c1
Gaute Meek Olsen <gaute.meek@gmail.com>
2026-03-25 12:51:35 +0100

    style and stepper

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
+import { CarOutlined, CheckSquareOutlined, FormOutlined, UserOutlined } from '@ant-design/icons'
+import { Steps } from 'antd'
+import { useState } from 'react'
+
 export function App() {
+  const [step, setStep] = useState(0)
   return (
     <>
       <header>
         <h1>Car insurance for you</h1>
       </header>
-      <main>...</main>
+      <main>
+        <div className="container">
+          <Steps
+            current={step}
+            items={[
+              {
+                title: 'Customer',
+                icon: <UserOutlined />,
+              },
+              {
+                title: 'Vehicle',
+                icon: <CarOutlined />,
+              },
+              {
+                title: 'Insurance',
+                icon: <FormOutlined />,
+              },
+              {
+                title: 'Summary',
+                icon: <CheckSquareOutlined />,
+              },
+            ]}
+          />
+          content
+        </div>
+      </main>
       <footer>
         <p>If you need help, call us!</p>
       </footer>
