f69b37a7ef3c0838b2937eb4b0b080cfae59ea1a
Gaute Meek Olsen <gaute.meek@gmail.com>
2026-04-02 13:30:50 +0200

    refactor playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,12 +2,11 @@ import { defineConfig, devices } from '@playwright/experimental-ct-react'
 
 export default defineConfig({
   testDir: './',
-  testMatch: '**\/*.spec.tsx',
+  testMatch: '**\/*.playwright.tsx',
   timeout: 10_000,
   fullyParallel: true,
   use: {
     ctPort: 3100,
-    headless: false,
     actionTimeout: 2_000,
   },
   projects: [
