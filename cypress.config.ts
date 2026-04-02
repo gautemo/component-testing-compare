0ed20baeb4a0110c19d2557e674e477101ceaa5d
Gaute Meek Olsen <gaute.meek@gmail.com>
2026-04-02 19:16:47 +0200

    ci tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,23 @@
 import { defineConfig } from 'cypress'
+import { mergeConfig, type Plugin } from 'vite'
+import viteConfig from './vite.config'
+
+function stripLegacyCypressOptimizeDepsPlugin(): Plugin {
+  return {
+    name: 'cypress-vite8-compat',
+    config(config) {
+      if (config?.optimizeDeps?.esbuildOptions) {
+        delete config.optimizeDeps.esbuildOptions
+      }
+    },
+  }
+}
+
+// dependencies that need to be the same instance between support file and test
+const CYPRESS_DEDUPE_DEPS = ['react', 'react-dom']
+
+// whatever triggers rebuilds due to the new optimized dependencies found
+const CYPRESS_PREBUNDLE_DEPS = ['@tanstack/react-query', 'antd', 'cypress/react', 'valtio']
 
 export default defineConfig({
   allowCypressEnv: false,
@@ -7,6 +26,15 @@ export default defineConfig({
     devServer: {
       framework: 'react',
       bundler: 'vite',
+      viteConfig: mergeConfig(viteConfig, {
+        plugins: [stripLegacyCypressOptimizeDepsPlugin()],
+        resolve: {
+          dedupe: CYPRESS_DEDUPE_DEPS,
+        },
+        optimizeDeps: {
+          include: CYPRESS_PREBUNDLE_DEPS,
+        },
+      }),
     },
   },
 })
--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,6 @@
-import { defineConfig, type Plugin } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-function stripLegacyCypressOptimizeDepsPlugin(): Plugin {
-  return {
-    name: 'cypress-vite8-compat',
-    config(config) {
-      if (config?.optimizeDeps?.esbuildOptions) {
-        delete config.optimizeDeps.esbuildOptions
-      }
-    },
-  }
-}
-
-// dependencies that need to be the same instance between support file and test
-const CYPRESS_DEDUPE_DEPS = ['react', 'react-dom']
-
-// whatever triggers rebuilds due to the new optimized dependencies found
-const CYPRESS_PREBUNDLE_DEPS = ['@tanstack/react-query', 'antd', 'cypress/react', 'valtio']
-
 export default defineConfig({
-  plugins: [react(), stripLegacyCypressOptimizeDepsPlugin()],
-  resolve: {
-    dedupe: CYPRESS_DEDUPE_DEPS,
-  },
-  optimizeDeps: {
-    include: CYPRESS_PREBUNDLE_DEPS,
-  },
+  plugins: [react()],
 })
